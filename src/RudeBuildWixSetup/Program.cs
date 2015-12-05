e87d0dd9be5b6b2f659e411a285d9cb60118310e
Martin Ecker <me@martinecker.com>
2015-12-04 16:26:29 -0800

    Implement IOleMessageFilter to fix spurious RPC_E_SERVERCALL_RETRYLATER errors from RemoveCommands() when calling into devenv to remove the registered RudeBuild commands. Doesn't work all the time unfortunately.

--- src/RudeBuildWixSetup/Program.cs
+++ src/RudeBuildWixSetup/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Xml.Linq;
 using System.Xml;
+using System.Runtime.InteropServices;
 using Microsoft.Win32;
 using Microsoft.Deployment.WindowsInstaller;
 using WixSharp;
@@ -263,6 +264,8 @@ public class CustomActions
             if (null == application)
                 return;
 
+            MessageFilter.Register();
+
             var connect = new RudeBuildAddIn.Connect();
             connect.OnUninstall(application);
         }
@@ -276,6 +279,7 @@ public class CustomActions
             if (null != application)
             {
                 application.Quit();
+                MessageFilter.Revoke();
             }
         }
     }
@@ -371,4 +375,73 @@ public class CustomActions
 
         return ActionResult.Success;
     }
+}
+
+// The following message filter class is from https://msdn.microsoft.com/en-us/library/ms228772.aspx 
+//   "How to: Fix 'Application is Busy' and 'Call was Rejected By Callee' Errors"
+// to fix spurious RPC_E_SERVERCALL_RETRYLATER errors from RemoveCommands() when calling into devenv
+// to remove the registered RudeBuild commands.
+
+// Class containing the IOleMessageFilter thread error-handling functions.
+public class MessageFilter : IOleMessageFilter
+{
+    // Start the filter.
+    public static void Register()
+    {
+        IOleMessageFilter newFilter = new MessageFilter();
+        IOleMessageFilter oldFilter = null;
+        CoRegisterMessageFilter(newFilter, out oldFilter);
+    }
+
+    // Done with the filter, close it.
+    public static void Revoke()
+    {
+        IOleMessageFilter oldFilter = null;
+        CoRegisterMessageFilter(null, out oldFilter);
+    }
+
+    // Handle incoming thread requests.
+    int IOleMessageFilter.HandleInComingCall(int dwCallType, System.IntPtr hTaskCaller, int dwTickCount, System.IntPtr lpInterfaceInfo)
+    {
+        //Return the flag SERVERCALL_ISHANDLED.
+        return 0;
+    }
+
+    // Thread call was rejected, so try again.
+    int IOleMessageFilter.RetryRejectedCall(System.IntPtr hTaskCallee, int dwTickCount, int dwRejectType)
+    {
+        if (dwRejectType == 2)
+        // flag = SERVERCALL_RETRYLATER.
+        {
+            // Retry the thread call immediately if return >=0 & 
+            // <100.
+            return 99;
+        }
+        // Too busy; cancel call.
+        return -1;
+    }
+
+    int IOleMessageFilter.MessagePending(System.IntPtr hTaskCallee, int dwTickCount, int dwPendingType)
+    {
+        //Return the flag PENDINGMSG_WAITDEFPROCESS.
+        return 2;
+    }
+
+    // Implement the IOleMessageFilter interface.
+    [DllImport("Ole32.dll")]
+    private static extern int CoRegisterMessageFilter(IOleMessageFilter newFilter, out IOleMessageFilter oldFilter);
+}
+
+[ComImport(), Guid("00000016-0000-0000-C000-000000000046"),
+InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+interface IOleMessageFilter
+{
+    [PreserveSig]
+    int HandleInComingCall(int dwCallType, IntPtr hTaskCaller, int dwTickCount, IntPtr lpInterfaceInfo);
+
+    [PreserveSig]
+    int RetryRejectedCall(IntPtr hTaskCallee, int dwTickCount, int dwRejectType);
+
+    [PreserveSig]
+    int MessagePending(IntPtr hTaskCallee, int dwTickCount, int dwPendingType);
 }
