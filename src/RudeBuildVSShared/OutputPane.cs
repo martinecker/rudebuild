31c4ef92d1839c3776e3a2fa91e19134fcb5b7d7
Martin Ecker <me@martinecker.com>
2015-12-08 11:14:16 -0800

    Support delay-initializing the output pane in case the VSPackage gets loaded so early that the DTE's windows array hasn't been setup yet.

--- src/RudeBuildVSShared/OutputPane.cs
+++ src/RudeBuildVSShared/OutputPane.cs
@@ -5,43 +5,63 @@ namespace RudeBuildVSShared
 {
 	public class OutputPane : IOutput
 	{
-        private readonly EnvDTE.Window _window;
-        private readonly EnvDTE.OutputWindow _outputWindow;
-		private readonly EnvDTE.OutputWindowPane _outputPane;
+		private readonly EnvDTE80.DTE2 _application;
+		private readonly string _outputPaneName;
+
+		private EnvDTE.Window _window;
+        private EnvDTE.OutputWindow _outputWindow;
+		private EnvDTE.OutputWindowPane _outputPane;
 
         public OutputPane(EnvDTE80.DTE2 application, string name)
 		{
-            _window = application.Windows.Item(EnvDTE.Constants.vsWindowKindOutput);
-            if (null == _window)
-                return;
-            _outputWindow = _window.Object as EnvDTE.OutputWindow;
-            if (null == _outputWindow)
-                return;
-            var outputWindowPane = from EnvDTE.OutputWindowPane pane in _outputWindow.OutputWindowPanes
-                                   where pane.Name == name
-                                   select pane;
-            _outputPane = outputWindowPane.SingleOrDefault();
-            if (_outputPane == null)
-            {
-                _outputPane = _outputWindow.OutputWindowPanes.Add(name);
-            }
+			_application = application;
+			_outputPaneName = name;
+
+			Initialize();
+		}
+
+		private bool Initialize()
+		{
+			if (null != _outputPane)	// already initialized?
+				return true;
+
+			if (0 == _application.Windows.Count)
+				return false;
+			_window = _application.Windows.Item(EnvDTE.Constants.vsWindowKindOutput);
+			if (null == _window)
+				return false;
+
+			_outputWindow = _window.Object as EnvDTE.OutputWindow;
+			if (null == _outputWindow)
+				return false;
+
+			var outputWindowPane = from EnvDTE.OutputWindowPane pane in _outputWindow.OutputWindowPanes
+								   where pane.Name == _outputPaneName
+								   select pane;
+			_outputPane = outputWindowPane.SingleOrDefault();
+			if (_outputPane == null)
+			{
+				_outputPane = _outputWindow.OutputWindowPanes.Add(_outputPaneName);
+			}
+
+			return _outputPane != null;
 		}
 
 		public void WriteLine(string line)
 		{
-            if (null != _outputPane)
+            if (Initialize())
                 _outputPane.OutputString(line + "\r\n");
 		}
 
         public void WriteLine()
         {
-            if (null != _outputPane)
+            if (Initialize())
                 _outputPane.OutputString("\r\n");
         }
 
 		public void Activate()
 		{
-            if (null != _outputPane)
+            if (Initialize())
             {
                 _outputPane.Activate();
                 _window.Activate();
@@ -50,7 +70,7 @@ namespace RudeBuildVSShared
 
 		public void Clear()
 		{
-            if (null != _outputPane) 
+            if (Initialize()) 
                 _outputPane.Clear();
 		}
 	}
