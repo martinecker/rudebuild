82534f042013836101a3cbf22b52de719c876660
Martin Ecker <me@martinecker.com>
2015-12-10 17:48:01 -0800

    Remove the old way of excluding files from unity builds via the separate SolutionSettingsAddExcludedCppFileNameDialog. Add context menus to projects and folders in the new projects tree view to allow expanding and collapsing all items recursively.

--- src/RudeBuildVSShared/SolutionSettingsDialog.xaml.cs
+++ src/RudeBuildVSShared/SolutionSettingsDialog.xaml.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell.Interop;
@@ -368,7 +365,6 @@ namespace RudeBuildVSShared
         private readonly SolutionInfo _solutionInfo;
         private readonly SolutionSettings _solutionSettings;
 
-		private TreeViewItem _selectedTreeViewItem;
 		private SolutionHierarchy _solutionHierarchy;
 
         public SolutionSettingsDialog(CommandManager commandManager, Settings settings, SolutionInfo solutionInfo)
@@ -386,6 +382,23 @@ namespace RudeBuildVSShared
 			RefreshProjectsTreeView();
         }
 
+		private void ExpandOrCollapseTreeViewItemRecursively(TreeViewItem treeViewItem, bool expand)
+		{
+			treeViewItem.IsExpanded = expand;
+			foreach (TreeViewItem childTreeViewItem in treeViewItem.Items)
+				ExpandOrCollapseTreeViewItemRecursively(childTreeViewItem, expand);
+		}
+
+		private void AddExpandAndCollapseAllContextMenuItems(ContextMenu contextMenu, TreeViewItem treeViewItem)
+		{
+			var contextMenuItemExpandAll = new MenuItem() { Header = "Expand All" };
+			var contextMenuItemCollapseAll = new MenuItem() { Header = "Collapse All" };
+			contextMenuItemExpandAll.Click += (sender, eventArgs) => { ExpandOrCollapseTreeViewItemRecursively(treeViewItem, true); };
+			contextMenuItemCollapseAll.Click += (sender, eventArgs) => { ExpandOrCollapseTreeViewItemRecursively(treeViewItem, false); };
+			contextMenu.Items.Add(contextMenuItemExpandAll);
+			contextMenu.Items.Add(contextMenuItemCollapseAll);
+		}
+
 		private void AddProjectsTreeViewItem(ItemCollection items, string projectName, IList<SolutionHierarchy.Item> projectItems)
 		{
 			ProjectInfo projectInfo = _solutionInfo.GetProjectInfo(projectName);
@@ -393,6 +406,8 @@ namespace RudeBuildVSShared
 				return;
 
 			var treeViewItem = new TreeViewItem() { FontSize = 11, FontWeight = FontWeights.Bold };
+			treeViewItem.ContextMenu = new ContextMenu();
+			AddExpandAndCollapseAllContextMenuItems(treeViewItem.ContextMenu, treeViewItem);
 
 			var image = new Image() { Source = _solutionHierarchy.GetIcon(SolutionHierarchy.IconType.Project) };
 			var label = new Label() { Content = projectName };
@@ -416,6 +431,8 @@ namespace RudeBuildVSShared
 		private void AddFolderTreeViewITem(ProjectInfo projectInfo, ItemCollection items, string folderName, IList<SolutionHierarchy.Item> folderItems)
 		{
 			var treeViewItem = new TreeViewItem() { FontWeight = FontWeights.Normal };
+			treeViewItem.ContextMenu = new ContextMenu();
+			AddExpandAndCollapseAllContextMenuItems(treeViewItem.ContextMenu, treeViewItem);
 
 			var image = new Image() { Source = _solutionHierarchy.GetIcon(SolutionHierarchy.IconType.Folder) };
 			var label = new Label() { Content = folderName };
@@ -480,11 +497,6 @@ namespace RudeBuildVSShared
 			stack.Children.Add(label);
 			treeViewItem.Header = stack;
 
-			/*			treeViewItem.PreviewMouseRightButtonDown += (sender, eventArgs)
-						{
-
-						};*/
-
 			items.Add(treeViewItem);
 		}
 
@@ -507,111 +519,6 @@ namespace RudeBuildVSShared
         private void OnCancel(object sender, RoutedEventArgs e)
         {
             DialogResult = false;
-        }
-
-        private void OnTreeViewItemSelected(object sender, RoutedEventArgs e)
-        {
-            _selectedTreeViewItem = e.OriginalSource as TreeViewItem;
-        }
-
-        private void OnTreeViewItemUnselected(object sender, RoutedEventArgs e)
-        {
-            _selectedTreeViewItem = null;
-        }
-		
-		private void OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
-			var sh = new SolutionHierarchy(_commandManager, _settings);
-
-			var clickedControl = e.OriginalSource as DependencyObject;
-            if (null != clickedControl)
-            {
-                var treeViewItem = clickedControl.VisualUpwardSearch<TreeViewItem>();
-                if (null != treeViewItem)
-                {
-                    treeViewItem.Focus();
-                    e.Handled = true;
-                }
-            }
-        }
-
-        private string GetSelectedFileName()
-        {
-            if (null == _treeViewExcludedFileNames.SelectedItem)
-                return null;
-            string fileName = _treeViewExcludedFileNames.SelectedItem as string;
-            if (string.IsNullOrEmpty(fileName))
-                return null;
-            return fileName;
-        }
-
-        private ProjectInfo GetProjectInfoFromTreeViewItem(TreeViewItem projectTreeViewItem)
-        {
-            var textBlock = projectTreeViewItem.VisualDownwardSearch<TextBlock>();
-            if (null == textBlock)
-                return null;
-            string projectName = textBlock.Text;
-            ProjectInfo projectInfo = _solutionInfo.GetProjectInfo(projectName);
-            if (null == projectInfo)
-                return null;
-            return projectInfo;
-        }
-
-        private void RefreshTreeViewBinding()
-        {
-            BindingOperations.ClearBinding(_treeViewExcludedFileNames, ItemsControl.ItemsSourceProperty);
-            _treeViewExcludedFileNames.SetBinding(ItemsControl.ItemsSourceProperty, new Binding("ProjectNameToExcludedCppFileNameMap"));
-        }
-
-        private void OnAddExcludedCppFileNameForProject(object sender, RoutedEventArgs e)
-        {
-            if (null == _selectedTreeViewItem)
-                return;
-            ProjectInfo projectInfo = GetProjectInfoFromTreeViewItem(_selectedTreeViewItem);
-            if (null == projectInfo)
-                return;
-
-            var dialog = new SolutionSettingsAddExcludedCppFileNameDialog(projectInfo, _solutionSettings);
-            try
-            {
-                dialog.ShowDialog();
-
-                if (dialog.DialogResult == true)
-                {
-                    foreach (string fileName in dialog.FileNamesToExclude)
-                    {
-                        _solutionSettings.ExcludeCppFileNameForProject(projectInfo, fileName);
-                    }
-                    RefreshTreeViewBinding();
-                }
-            }
-            finally
-            {
-                dialog.Close();
-            }
-        }
-
-        private void OnRemoveExcludedCppFileNameForProject(object sender, RoutedEventArgs e)
-        {
-            if (null == _selectedTreeViewItem)
-                return;
-
-            string fileName = GetSelectedFileName();
-            if (null == fileName)
-                return;
-
-            DependencyObject parentControl = VisualTreeHelper.GetParent(_selectedTreeViewItem);
-            if (null == parentControl)
-                return;
-            var projectTreeViewItem = parentControl.VisualUpwardSearch<TreeViewItem>();
-            if (null == projectTreeViewItem)
-                return;
-            ProjectInfo projectInfo = GetProjectInfoFromTreeViewItem(projectTreeViewItem);
-            if (null == projectInfo)
-                return;
-
-            _solutionSettings.RemoveExcludedCppFileNameForProject(projectInfo, fileName);
-            RefreshTreeViewBinding();
         }
     }
 }
